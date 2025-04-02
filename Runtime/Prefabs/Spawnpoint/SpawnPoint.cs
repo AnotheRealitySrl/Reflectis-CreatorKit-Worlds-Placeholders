5bf6196d17f87ca43e8c46e4fec5c5675ddd348b
Arry-93 <arry93@hotmail.it>
2025-04-02 18:48:00 +0200

    prefabs and script placeholders reorganization

---
+++ Runtime/Prefabs/Spawnpoint/SpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Reflectis.CreatorKit.Worlds.Core.Placeholders
+{
+    public class SpawnPoint : MonoBehaviour
+    {
+        private readonly List<Collider> collidingObjs = new();
+
+        public bool IsFree => collidingObjs.Count == 0;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            collidingObjs.Add(other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            collidingObjs.Remove(other);
+        }
+    }
+}
+
+
