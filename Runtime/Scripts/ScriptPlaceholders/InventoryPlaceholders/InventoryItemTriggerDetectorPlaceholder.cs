5ac462b00da3b72f8616e7b0aca9c7d36516a616
Francesco Weikmann <fweikmann@gmail.com>
2025-07-24 11:19:54 +0200

    add logic to handle different colliders on trigger

--- Runtime/Scripts/ScriptPlaceholders/InventoryPlaceholders/InventoryItemTriggerDetectorPlaceholder.cs
+++ Runtime/Scripts/ScriptPlaceholders/InventoryPlaceholders/InventoryItemTriggerDetectorPlaceholder.cs
@@ -1,4 +1,7 @@
 using Reflectis.CreatorKit.Worlds.Core.Placeholders;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,10 +12,38 @@ namespace Reflectis.CreatorKit.Worlds.Placeholders
         [Tooltip("The reference to the item that triggers the action when entering the collider")]
         public InventoryItemPlaceholder inventoryItem;
 
+        [Tooltip("Whether or not only a specific collider of the inventory item can trigger. Leave to false if all the colliders can trigger the detector")]
+        public bool handleSpecificColliderPosition = false;
+        [HideInInspector] public int colliderTriggerIndex = -1;
+
         [Tooltip("The collider in the scene that is triggered when the inventory item enters its space")]
-        public Collider[] detectorCollider; 
+        public Collider[] detectorCollider;
 
         public UnityEvent OnItemTriggerEnter;
         public UnityEvent OnItemTriggerExit;
     }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(InventoryItemTriggerDetectorPlaceholder))]
+    public class InventoryItemTriggerDetectorPlaceholderEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            DrawDefaultInspector();
+
+            // Draw the "consumable" field
+            SerializedProperty specificColl = serializedObject.FindProperty("handleSpecificColliderPosition");
+
+            // Conditionally show "numberOfUses"
+            if (specificColl.boolValue)
+            {
+                SerializedProperty triggerIndex = serializedObject.FindProperty("colliderTriggerIndex");
+                EditorGUILayout.PropertyField(triggerIndex);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+#endif
 }
