69b73b15f1927ba560260ddbfac367909b01ecb4
Francesco Weikmann <fweikmann@gmail.com>
2026-04-23 15:59:48 +0200

    add descriptionKey Placeholder

--- Runtime/Scripts/ScriptPlaceholders/TaskPlaceholder/TaskUIPlaceholder.cs
+++ Runtime/Scripts/ScriptPlaceholders/TaskPlaceholder/TaskUIPlaceholder.cs
@@ -5,5 +5,6 @@ namespace Reflectis.CreatorKit.Worlds.Placeholders
     public class TaskUIPlaceholder : SpawnableHandlerPlaceholder
     {
         [SerializeField] public GameObject taskSystem; //Reference to task system for callbacks (Forse non serve, ma ci collego gli eventi di callback)
+        [SerializeField] public string descriptionKey;
     }
 }
