bd3e01a989b8ae5d5824092af3839482a32f6891
Francesco Weikmann <fweikmann@gmail.com>
2026-06-04 17:06:23 +0200

    add boolean to avoid remove of wearables

--- Runtime/Scripts/ScriptPlaceholders/PickablePlaceholder/WearablePlaceholder.cs
+++ Runtime/Scripts/ScriptPlaceholders/PickablePlaceholder/WearablePlaceholder.cs
@@ -1,3 +1,4 @@
+using PlasticPipe.PlasticProtocol.Messages;
 using UnityEngine;
 
 namespace Reflectis.CreatorKit.Worlds.Placeholders
@@ -5,5 +6,7 @@ namespace Reflectis.CreatorKit.Worlds.Placeholders
     [RequireComponent(typeof(PickablePlaceholder))]
     public class WearablePlaceholder : SceneComponentPlaceholderNetwork
     {
+        [Tooltip("If set to false then the user cannot unequip this wearable")]
+        public bool canRemove = true;
     }
 }
